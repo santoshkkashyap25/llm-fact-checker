eb83214069b405e7f2af3432c62a5569ab55e2d3
santoshkkashyap25 <santoshkkashyap25@gmail.com>
2025-06-11 20:41:54 +0530

    Update config

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
 FACTS_CSV_PATH = DATA_DIR / "trusted_facts.csv"
 VECTOR_INDEX_PATH = DATA_DIR / "faiss_index.bin"
-PIB_RSS_URL = "https://www.pib.gov.in/ViewRss.aspx"
 
 EMBEDDING_MODEL = 'all-mpnet-base-v2'
 
